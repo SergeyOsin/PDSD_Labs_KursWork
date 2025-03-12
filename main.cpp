7acbc8906036d408024cd2de6647de31b2f9d8c6
SergeyOsin2005 <sereosi2017@yandex.ru>
2025-03-12 15:37:21 +0300

    LB5 ready (maybe)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "SetStruct.h"
 #include "SetClass.h"
 #include "SetList.h"
+#include "Setset.h"
 using namespace std;
 const int min_size = 7, max_size = 9;
 const int min_elem = 10, max_elem = 98;
@@ -12,14 +13,17 @@ int main() {
 	cout << "Множество A: ";
 	int sizeA = rand() % (max_size - min_size + 1) + min_size;
 	int sizeB = rand() % (max_size - min_size + 1) + min_size;
+	cout << "Структура SET:\n";
 	SetStruct* A = createnewSet('A', sizeA, min_elem, max_elem);
 	cout << printSet(A, ',') << '\n';
 	cout << "Мощность множества A: " << LengthSet(A) << '\n';
+	cout << "Класс SET:\n";
 	SetClass* SA = new SetClass();
 	SA->createnewSet('A', sizeA, min_elem, max_elem);
 	cout << "Множество A: ";
 	cout << SA->printSet(',') << '\n';
 	cout << "Мощность А: " << SA->LengthSet() << '\n';
+	cout << "Контейнер list:\n";
 	SetList* setlist = new SetList();
 	setlist=setlist->createnewSet('A', sizeA, min_elem, max_elem);
 	cout << "Множество A: " << setlist->printSet(',');
@@ -32,6 +36,11 @@ int main() {
 	SetList* C = setlist->SimmetricDif(setlist1);
 	cout << C->printSet(',') << '\n';
 	cout << "Мощность C: " << C->LengthSet();
+	cout << "\nКонтейнер set:\n";
+	Setset* set1 = new Setset();
+	set1=set1->createnewSet('A', sizeA, min_elem, max_elem);
+	cout << "Множество A: " << set1->printSet(',') << '\n';
+	cout << "Мощность A: " << set1->LengthSet();
 	return 0;
 
 }
