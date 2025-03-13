2fb2378831df58adc08009081130ea859a8616b3
SergeyOsin2005 <sereosi2017@yandex.ru>
2025-03-13 18:15:20 +0300

    ready but not bitset

--- Setbitset.h
+++ Setbitset.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <bitset>
+#include <string>
+
+class Setbitset {
+private:
+	std::bitset<100000>bitset0;
+public:
+	Setbitset();
+	bool isEmptySet();
+	bool isElementinSet(int element);
+	void addnewElement(int new_element);
+	Setbitset* createnewSet(char A, int size, int min_element, int max_element);
+	int LengthSet();
+	std::string printSet(char delimiter);
+	void clearSet();
+	bool isSubset(Setbitset* SecondB);
+	bool isEqual(Setbitset* SecondB);
+	Setbitset* merge(Setbitset* SecondB);
+	Setbitset* Intersection(Setbitset* SecondB);
+	Setbitset* Difference(Setbitset* SecondB);
+	Setbitset* SimmetricDif(Setbitset* SecondB);
+};
---
+++ Setunorderedset.cpp
@@ -0,0 +1,109 @@
+#include "Setunorderedset.h"
+//F1. Создание пустого множества
+Setunorderedset::Setunorderedset() {
+
+}
+
+//F2. 
+bool Setunorderedset::isEmptySet() {
+	return set1.empty();
+}
+
+//F3. 
+bool Setunorderedset::isElementinSet(int element) {
+	return set1.count(element) > 0;
+}
+
+//F4.
+void Setunorderedset::addnewElement(int new_element) {
+	if (!isElementinSet(new_element))
+		set1.insert(new_element);
+}
+
+//F5.
+Setunorderedset* Setunorderedset::createnewSet(char A, int size, int min_element, int max_element) {
+	Setunorderedset* newset = new Setunorderedset();
+	if (size <= 0 || min_element >= max_element)
+		return newset;
+	int count_elem = 0;
+	while (count_elem < size) {
+		int rand_elem = rand() % (max_element - min_element + 1) + min_element;
+		if (A == 'A') {
+			if (rand_elem % 2 == 0)
+				rand_elem++;
+		}
+		else if (rand_elem % 3 != 0)
+			rand_elem += 3 - rand_elem % 3;
+		newset->addnewElement(rand_elem);
+		count_elem++;
+	}
+	return newset;
+}
+
+//F6.
+int Setunorderedset::LengthSet() {
+	return set1.size();
+}
+
+//F7.
+std::string Setunorderedset::printSet(char delimiter) {
+	if (isEmptySet())
+		return " ";
+	std::string printing_set = "";
+	for (auto iter = set1.begin(); iter != set1.end(); iter++)
+		printing_set += std::to_string(*iter) + delimiter;
+	printing_set.pop_back();
+	return printing_set;
+}
+
+//F8. 
+void Setunorderedset::clearSet() {
+	set1.clear();
+}
+
+//F9.
+bool Setunorderedset::isSubset(Setunorderedset* SecondB) {
+	for (auto iter = set1.begin(); iter != set1.end(); iter++) {
+		if (!SecondB->isElementinSet(*iter))
+			return false;
+	}
+	return true;
+}
+
+//F10
+bool Setunorderedset::isEqual(Setunorderedset* SecondB) {
+	return this->isSubset(SecondB) && SecondB->isSubset(this);
+}
+
+//F11
+Setunorderedset* Setunorderedset::merge(Setunorderedset* SecondB) {
+	Setunorderedset* C = new Setunorderedset();
+	for (auto iteri = set1.begin(); iteri != set1.end(); iteri++)
+		C->addnewElement(*iteri);
+	for (auto iterj = SecondB->set1.begin(); iterj != SecondB->set1.end(); iterj++)
+		C->addnewElement(*iterj);
+	return C;
+}
+
+//F12
+Setunorderedset* Setunorderedset::Intersection(Setunorderedset* SecondB) {
+	Setunorderedset* C = new Setunorderedset();
+	for (auto iteri = set1.begin(); iteri != set1.end(); iteri++)
+		if (SecondB->isElementinSet(*iteri))
+			C->addnewElement(*iteri);
+	return C;
+}
+
+//F13
+Setunorderedset* Setunorderedset::Difference(Setunorderedset* SecondB) {
+	Setunorderedset* C = new Setunorderedset();
+	for (auto iteri = set1.begin(); iteri != set1.end(); iteri++)
+		if (!SecondB->isElementinSet(*iteri))
+			C->addnewElement(*iteri);
+	return C;
+}
+
+//F14
+Setunorderedset* Setunorderedset::SimmetricDif(Setunorderedset* SecondB) {
+	return this->merge(SecondB)->Difference(this->Intersection(SecondB));
+}
