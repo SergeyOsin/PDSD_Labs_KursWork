c902b5977bdd1acbc028c55d82c4a5b90ef6de9b
SergeyOsin2005 <sereosi2017@yandex.ru>
2025-03-25 20:55:37 +0300

    ready lab5

--- main.cpp
+++ main.cpp
@@ -2,17 +2,20 @@
 #include <iomanip>
 #include "SetStruct.h"
 #include "SetClass.h"
-#include "SetList.h"
+#include "Setset.h"
 #include "Setset.h"
 #include "Setunorderedset.h"
 #include "Setbitset.h"
 #include <Windows.h>
+#include <time.h>
+#define nl '\n'
 using namespace std;
 const int min_size = 7, max_size = 9;
 const int min_elem = 10, max_elem = 98;
-int SIZE_SET;
+const string horizontLine = "\n---------------------------------------------------------------------------------------------------------------------\n";
+int setSize;
 void lineWithParam(string text) {
-	string horizontLine = "\n---------------------------------------------------------------------------------------------------------------------\n";
+	horizontLine;
 	while (text.size() < 23) text += " ";
 	string emptyCell1(20, ' ');
 	string emptyCell2(14, ' ');
@@ -25,7 +28,20 @@ void lineWithParam(string text) {
 		'|' + emptyCell3 + '|' + emptyCell4 + '|' + emptyCell5 +
 		'|' + emptyCell6 + '|';
 }
-
+void emptycell(int num,string text) {
+	string empty(num, ' ');
+	cout << empty << text;
+}
+void buildSetStruct() {
+	cout << horizontLine;
+	emptycell(6, "Создание множества|");
+	SetStruct* set0 = new SetStruct();
+	clock_t clock0 = clock();
+	set0 = createnewSet('A', setSize, 0, setSize * 10);
+	clock_t finish = clock();
+	double rez =(double) (finish - clock0) / CLOCKS_PER_SEC;
+	cout << rez;
+}
 void addTitle() {
 	const string horizontLine = "\n---------------------------------------------------------------------------------------------------------------------\n";
 	const string title = horizontLine + "\t\t\t| Односвязный список | Класс список |    List    |     Set     |  Bitset  | Unordered_set |";
@@ -50,64 +66,68 @@ void addAllLineWithParam() {
 }
 
 void createTable() {
-	cout << "Введите размер множества: \n";
-	addAllLineWithParam();
+	cout << "Введите размер множества: ";
+	cin>>setSize;
+	cout << '\n';
+	addTitle();
+	buildSetStruct();
 }
+
+
+
 int main() {
 	setlocale(LC_ALL, "ru");
 	SetConsoleTitle(L"Осин Сергей 23ВП2");
+	/*Lab5();*/
 	createTable();
-	/*cout << "Осин Сергей. 23ВП2. Вариант-21. Множество A - нечётные цифры, Множество B - числа кратные 3.\n";
-	srand(time(NULL));
-	cout << "Множество A: ";
-	int sizeA = rand() % (max_size - min_size + 1) + min_size;
-	int sizeB = rand() % (max_size - min_size + 1) + min_size;
-	cout << "Структура set3:\n";
-	SetStruct* A = createnewSet('A', sizeA, min_elem, max_elem);
-	cout << printSet(A, ',') << '\n';
-	cout << "Мощность множества A: " << LengthSet(A) << '\n';
-	cout << "Класс set3:\n";
-	SetClass* SA = new SetClass();
-	SA->createnewSet('A', sizeA, min_elem, max_elem);
-	cout << "Множество A: ";
-	cout << SA->printSet(',') << '\n';
-	cout << "Мощность А: " << SA->LengthSet() << '\n';
-	cout << "Контейнер list:\n";
-	SetList* setlist = new SetList();
-	setlist=setlist->createnewSet('A', sizeA, min_elem, max_elem);
-	cout << "Множество A: " << setlist->printSet(',');
-	cout << "\nМощность A: " << setlist->LengthSet();
-	cout << "\nМножество B: ";
-	SetList* setlist1 = new SetList();
-	setlist1 = setlist1->createnewSet('B', sizeB, min_elem, max_elem);
-	cout << setlist1->printSet(',');
-	cout << "\nСимметричная разность A и B: ";
-	SetList* C = setlist->SimmetricDif(setlist1);
-	cout << C->printSet(',') << '\n';
-	cout << "Мощность C: " << C->LengthSet();
-	cout << "\nКонтейнер set3:\n";
-	Setset* set1 = new Setset();
-	set1=set1->createnewSet('A', sizeA, min_elem, max_elem);
-	cout << "Множество A: " << set1->printSet(',') << '\n';
-	cout << "Мощность A: " << set1->LengthSet();
-	cout << "\nКонтейнер unorderedset:\n";
-	Setunorderedset* setB = new Setunorderedset();
-	setB = setB->createnewSet('A', sizeA, min_elem, max_elem);
-	cout << "Множество B: " << setB->printSet(',') << '\n';
-	cout << "Мощность B: " << setB->LengthSet() << '\n';
-	cout << "Контейнер bitset:\n";
-	Setbitset* set3 = new Setbitset();
-	set3 = set3->createnewSet('A', 10, 10, 99);
-	cout << "A: " << set3->printSet(',') + '\n';
-	cout << "length A: "<< set3->LengthSet() + '\n';
-	Setbitset* setb = new Setbitset();
-	setb = setb->createnewSet('B', 10, 11, 99);
-	cout << "B: " + setb->printSet(',') + '\n';
-	Setbitset* C1 = new Setbitset();
-	C1= set3->Intersection(setb);
-	cout << "C1: " + C1->printSet(',') + '\n';
-	cout << "Length C1: " << C1->LengthSet() + '\n';*/
 	system("pause");
 	return 0;
+}
 
+void Lab5() {
+	cout << "Лабораторная работа 5.\nОсин Сергей. 23ВП2. Вариант-21. Множество A - нечётные цифры, Множество B - числа кратные 3.\n";
+	srand(time(NULL));
+	int sizeA = rand() % (max_size - min_size + 1) + min_size;
+	int sizeB = rand() % (max_size - min_size + 1) + min_size;
+	cout << "Множество A: ";
+	Setset* setsetA = new Setset();
+	setsetA = setsetA->createnewSet('A', sizeA, min_elem, max_elem);
+	cout << setsetA->printSet(',') + '\n';
+	cout << "Мощность A: " << setsetA->LengthSet() << nl;
+	cout << "Множество B: ";
+	Setset* setsetB = new Setset();
+	setsetB = setsetB->createnewSet('B', sizeB, min_elem, max_elem);
+	cout << setsetB->printSet(',') + nl;
+	cout << "Мощность B: " << setsetB->LengthSet() << nl;
+	string state = setsetA->isSubset(setsetB) ? "Да" : "Нет";
+	cout << "Множество A подмножество B? " + state + nl;
+	state = setsetA->isSubset(setsetA) ? "Да" : "Нет";
+	cout << "Множество А подмножество В? " + state + nl;
+	state = setsetA->isEqual(setsetB) ? "Да" : "Нет";
+	cout << "Множество А равно B? " + state + nl;
+	state = setsetA->isEqual(setsetA) ? "Да" : "Нет";
+	cout << "Множеество А равно A? " + state + nl;
+	Setset* setsetC = new Setset();
+	setsetC = setsetA->merge(setsetB);
+	cout << "Объединение А и В: " << setsetC->printSet(',') + nl;
+	cout << "Мощность C: " << setsetC->LengthSet() << nl;
+	setsetC = setsetA->Intersection(setsetB);
+	cout << "Пересечение А и В: " << setsetC->printSet(',') + nl;
+	cout << "Мощность С: " << setsetC->LengthSet() << nl;
+	setsetC = setsetA->Difference(setsetB);
+	cout << "Разность А и В: " << setsetC->printSet(',') + nl;
+	cout << "Мощность С: " << setsetC->LengthSet() << nl;
+	setsetC = setsetB->Difference(setsetA);
+	cout << "Разность В и А: " << setsetC->printSet(',') + nl;
+	cout << "Мощность C: " << setsetC->LengthSet() << nl;
+	setsetC = setsetA->SimmetricDif(setsetB);
+	cout << "Симметричная разность A и B: " << setsetC->printSet(',') + nl;
+	cout << "Мощность С: " << setsetC->LengthSet() << nl;
+	cout << "Очистка множеств A и B: " << nl;
+	setsetA->clearSet();
+	setsetB->clearSet();
+	cout << "Множество А: " << setsetA->printSet(',') + nl;
+	cout << "Мощность А: " << setsetA->LengthSet() << nl;
+	cout << "Множество B: " << setsetB->printSet(',') + nl;
+	cout << "Мощность B: " << setsetB->LengthSet() << nl;
 }
